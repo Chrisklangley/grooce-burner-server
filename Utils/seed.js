394f2bce52a010a68377a7ccac1957dc9e12f6aa
Chrisklangley <christopherklangley@gmail.com>
2023-07-30 21:57:13 -0700

    new piece added

--- Utils/seed.js
+++ Utils/seed.js
@@ -5,7 +5,9 @@ const seedDB = async (req, res) => {
   const { MONGODB_CONNECTION } = process.env;
 
   try {
-    await mongoose.connect(MONGODB_CONNECTION);
+    await mongoose.connect(
+      "mongodb+srv://christopherlangleydev:Dq8XRo8Gh7K3ErCq@groverburnerdb.odla6k1.mongodb.net/"
+    );
     res.status(200).send("connected");
   } catch (error) {
     console.error("Connection error:", error);
